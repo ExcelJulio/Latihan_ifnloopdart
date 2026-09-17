81bad88d528df1611c28bd2e37688c19527885b7
Excel Julio <exceljulio3008@gmail.com>
2026-09-17 17:35:48 +0700

    versi 2

--- ifnloop.dart
+++ ifnloop.dart
@@ -1,13 +1,13 @@
 void main () {
   //     if
 
-    int nilai = 86;
-if (nilai > 85) {
-    print('nilai di atas 85');
-} else {
-    if (nilai < 85)
-    print('nilai di bawah 85');
-}
+//     int nilai = 86;
+// if (nilai > 85) {
+//     print('nilai di atas 85');
+// } else {
+//     if (nilai < 85)
+//     print('nilai di bawah 85');
+// } 
 
 // if (nilai > 85 ){
 //     print('nilai di atas 85');
@@ -15,18 +15,19 @@ if (nilai > 85) {
 // if(nilai < 85) {
 //     print('nilai di bawah 85');
 // }
+
 // switch
-// var nilai = 'A';
-// switch (nilai) {
-//     case 'A':
-//     print('nilai di atas adalah A');
-//     break;
-//     case 'B':
-//     print('nilai di atas bawah C');
-//     case 'C':
-//     print('nilai di atas bawah C');
-//     break;
-//     default:
-//     print('nilai diluar pilihan');
+var nilai = 'A';
+switch (nilai) {
+    case 'A':
+    print('nilai di atas adalah A');
+    break;
+    case 'B':
+    print('nilai di atas bawah B');
+    case 'C':
+    print('nilai di atas bawah C');
+    break;
+    default:
+    print('nilai diluar pilihan');}
 
 }
