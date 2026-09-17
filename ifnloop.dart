3deb67f697028c09d382fa038fcd935d49a8ea25
exceljulio <exceljulio3008@gmail.com>
2026-09-17 17:02:23 +0700

    versi 1

---
+++ ifnloop.dart
@@ -0,0 +1,32 @@
+void main () {
+  //     if
+
+    int nilai = 84;
+if (nilai > 85) {
+    print('nilai di atas 85');
+} else {
+    if (nilai < 85)
+    print('nilai di bawah 85');
+}    
+
+// if (nilai > 85 ){
+//     print('nilai di atas 85');
+// }
+// if(nilai < 85) {
+//     print('nilai di bawah 85');
+// }
+// switch
+// var nilai = 'A';
+// switch (nilai) {
+//     case 'A':
+//     print('nilai di atas adalah A');
+//     break;
+//     case 'B':
+//     print('nilai di atas bawah C');
+//     case 'C':
+//     print('nilai di atas bawah C');
+//     break;
+//     default:
+//     print('nilai diluar pilihan');
+
+}
